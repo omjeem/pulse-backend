7ce744ce79372f7254b7bf3abf788bc7b3a542f3
omjeem <omjeem558@gmail.com>
2025-12-18 00:03:09 +0530

    express setup

---
+++ src/config/envConfig.ts
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { z } from "zod";
+
+const envVarsSchema = z.object({
+  PORT: z
+    .string()
+    .transform((str) => parseInt(str, 10)),
+  DATABASE_URL: z.string(),
+  JWT_SECRET: z.string().nonempty(),
+  JWT_EXPIRES_IN: z
+    .string()
+    .nonempty()
+    .transform((str) => parseInt(str, 10)),
+});
+
+const envVars = envVarsSchema.parse(process.env);
+
+export const envConfigs = {
+  port: envVars.PORT,
+  jwt: {
+    secret: envVars.JWT_SECRET,
+    expiresIn: envVars.JWT_EXPIRES_IN,
+  },
+  database_url: envVars.DATABASE_URL,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World")
+import express from "express";
+import cors from "cors";
+import { envConfigs } from "./config/envConfig";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Welcome to pulse api!",
+  });
+});
+
+app.listen(envConfigs.port, () => {
+  console.log(`Server is listening on http://localhost:${envConfigs.port}`);
+});
